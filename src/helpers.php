b1dc4132b6ce2f70cb16118a08172ea5f4b366fd
Pedro Costa <costa@pixelsbrand.com>
2019-11-07 16:22:10 +0000

    Changed helpers function to most updated browsers support

--- src/helpers.php
+++ src/helpers.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Str;
 if (!function_exists('supportsWebp')) {
     function supportsWebp() {
         return Str::contains($request->headers->get('user_agent'), ['Trident','Safari',]) !== false;
+
     }
 }
